86b8e083bfea421a4f1bfcd642d4e9272459b6a3
Sil_Palaoro <silvinapalaoro@gmail.com>
2026-05-09 20:26:08 -0300

    Agrega modificar reserva

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -307,3 +307,29 @@ function calcularPrecioTotal() {
   );
 
 }
+
+// ======================================================
+// 6 - MODIFICAR RESERVA
+// ======================================================
+
+function modificarReserva() {
+
+  let id = parseInt(
+    prompt("ID reserva:")
+  );
+
+  let reserva = reservas.find(
+    r => r.id === id
+  );
+
+  if(!reserva) {
+    alert("Reserva no encontrada");
+    return;
+  }
+
+  reserva.entrada = prompt("Nueva fecha entrada:");
+  reserva.salida = prompt("Nueva fecha salida:");
+
+  alert("Reserva modificada");
+
+}
