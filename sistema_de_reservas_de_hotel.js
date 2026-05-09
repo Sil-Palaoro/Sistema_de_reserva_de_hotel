56b48abbc85556b49c5e73f22f754095dc529efd
Micaela Coradini <micacoradini@gmail.com>
2026-05-09 19:58:52 -0300

    Corrige error de codigo

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -213,7 +213,31 @@ function crearReserva() {
   alert("Reserva creada correctamente");
 
 }
-/ ======================================================
+// ======================================================
+// 2 - CONSULTAR DISPONIBILIDAD
+// ======================================================
+
+function consultarDisponibilidad() {
+
+  let mensaje = "HABITACIONES DISPONIBLES\n\n";
+
+  habitaciones.forEach(h => {
+
+    if(h.disponible) {
+
+      mensaje +=
+        "Habitación: " + h.numero +
+        " | Tipo: " + h.tipo + "\n";
+
+    }
+
+  });
+
+  alert(mensaje);
+
+}
+
+// ======================================================
 // 3 - SELECCIONAR HABITACION
 // ======================================================
 
