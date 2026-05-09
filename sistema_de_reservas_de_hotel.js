af078e5a4838653a13eaeb108e05b26b5bb33ddc
Micaela Coradini <micacoradini@gmail.com>
2026-05-09 19:50:41 -0300

    Agrega consulta de disponibilidad

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -213,3 +213,26 @@ function crearReserva() {
   alert("Reserva creada correctamente");
 
 }
+// ======================================================
+// 2 - CONSULTAR DISPONIBILIDAD
+// ======================================================
+
+function consultarDisponibilidad() {
+
+  let mensaje = "HABITACIONES DISPONIBLES\n\n";
+
+  habitaciones.forEach(h => {
+
+    if(h.disponible) {
+
+      mensaje +=
+        "Habitación: " + h.numero +
+        " | Tipo: " + h.tipo + "\n";
+
+    }
+
+  });
+
+  alert(mensaje);
+
+}
