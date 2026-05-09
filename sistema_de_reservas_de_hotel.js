0cf5fde5bdb6b5c920ca685eb5d8a3ce0aefe5e6
katymo <karentymo@gmail.com>
2026-05-09 20:59:27 -0300

    gestion de reserva

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -405,4 +405,29 @@ function registrarCliente() {
 
   alert("Cliente registrado");
 
+}// ======================================================
+// 11 - GESTION ESTADOS
+// ======================================================
+
+function gestionarEstados() {
+
+  let id = parseInt(prompt("ID reserva:"));
+
+  let reserva = reservas.find(
+    r => r.id === id
+  );
+
+  if(!reserva) {
+    alert("Reserva no encontrada");
+    return;
+  }
+
+  let estado = prompt(
+    "Nuevo estado:"
+  );
+
+  reserva.estado = estado;
+
+  alert("Estado actualizado");
+
 }
