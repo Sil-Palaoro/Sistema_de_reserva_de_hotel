f57994889641c0b3d1ae82966c037edd95dbe201
Micaela Coradini <micacoradini@gmail.com>
2026-05-09 19:53:20 -0300

    Agrega seleccion de habitacion

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -213,3 +213,26 @@ function crearReserva() {
   alert("Reserva creada correctamente");
 
 }
+/ ======================================================
+// 3 - SELECCIONAR HABITACION
+// ======================================================
+
+function seleccionarHabitacion() {
+
+  let mensaje = "SELECCIÓN HABITACIÓN\n\n";
+
+  habitaciones.forEach(h => {
+
+    mensaje +=
+      "Número: " + h.numero +
+      "\nTipo: " + h.tipo +
+      "\nPrecio: $" + h.precio +
+      "\nCapacidad: " + h.capacidad +
+      "\nServicios: " + h.servicios +
+      "\n------------------\n";
+
+  });
+
+  alert(mensaje);
+
+}
