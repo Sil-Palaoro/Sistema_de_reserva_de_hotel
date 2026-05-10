04c3e98903ad8d55acd444994260b57e5482d088
alupizarro <aannpp1974@gmail.com>
2026-05-09 21:41:12 -0300

    agregar gestion de pagos

--- sistema_de_reservas_de_hotel.js
+++ sistema_de_reservas_de_hotel.js
@@ -532,3 +532,29 @@ function checkOut() {
 
 }
 
+// ======================================================
+// 15 - GESTION PAGOS
+// ======================================================
+
+function gestionPagos() {
+
+  let id = parseInt(prompt("ID reserva:"));
+
+  let reserva = reservas.find(
+    r => r.id === id
+  );
+
+  if(!reserva) {
+    alert("Reserva no encontrada");
+    return;
+  }
+
+  let metodo = prompt(
+    "Método pago:"
+  );
+
+  reserva.metodoPago = metodo;
+
+  alert("Pago registrado");
+
+}
