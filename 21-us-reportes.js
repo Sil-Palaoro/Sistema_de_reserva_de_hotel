458ebbcbc4779a40ac29d795823c15affdd4133a
alupizarro <aannpp1974@gmail.com>
2026-05-13 20:28:22 -0300

    agregar reportes

---
+++ 21-us-reportes.js
@@ -0,0 +1,33 @@
+// ======================================================
+// 17 - REPORTES
+// ======================================================
+
+function reportes() {
+
+  alert(
+    "Total clientes: " + clientes.length +
+    "\nTotal reservas: " + reservas.length
+  );
+
+}
+
+// ======================================================
+// 18 - CONTROL DISPONIBILIDAD
+// ======================================================
+
+function controlDisponibilidad() {
+
+  let mensaje = "CONTROL DISPONIBILIDAD\n\n";
+
+  habitaciones.forEach(h => {
+
+    mensaje +=
+      "Habitación " + h.numero +
+      " -> Disponible: " +
+      h.disponible + "\n";
+
+  });
+
+  alert(mensaje);
+
+}
